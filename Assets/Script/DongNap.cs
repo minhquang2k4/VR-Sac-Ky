d92d41b603ae40db3209a52cfa5efd2d6df37cda
minhquang2k4 <minhwang1708@gmail.com>
2024-11-17 00:05:51 +0700

    update nap binh all

--- Assets/Script/DongNap.cs
+++ Assets/Script/DongNap.cs
@@ -6,16 +6,18 @@ using UnityEngine;
 public class DongNap : MonoBehaviour
 {
     [SerializeField] Transform _transform;
+    [SerializeField] float _valueY;
+    [SerializeField] string _tag;    
+
     Vector3 _position;
 
     void OnTriggerStay(Collider collision)
     {
-        if (collision.gameObject.CompareTag("PositionNapBinh"))
+        if (collision.gameObject.CompareTag(_tag))
         {
-            _position = new Vector3(_transform.position.x, _transform.position.y - 0.008f, _transform.position.z);
+            _position = new Vector3(_transform.position.x, _transform.position.y - _valueY, _transform.position.z);
             this.transform.position = _position;
-            this.transform.rotation = _transform.rotation; // Gán trực tiếp Quaternion
-            Debug.Log("Da cham vao nap binh");
+            this.transform.rotation = _transform.rotation;
         }
     }
 }
