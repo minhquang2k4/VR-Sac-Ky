36c51383ccb1eefb29588c380dd270a855b594f9
Pham Minh Nhat <phamminhnhat629@gmail.com>
2025-12-31 16:39:25 +0700

    add countdown func + sprites

--- Assets/Script/BinhChaySaky.cs
+++ Assets/Script/BinhChaySaky.cs
@@ -2,15 +2,74 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BinhChaySacky : MonoBehaviour
 {
 
-    [SerializeField ]MeshRenderer rend;
-    
+    [SerializeField] MeshRenderer rend;
+    [SerializeField] Canvas countdownCanvas;
+    [SerializeField] Image countdownImage;
+    [SerializeField] Sprite[] countdownSprites;
+
+    private bool isCountdownActive = false;
+
+
+    public void Start()
+    {
+        if (countdownCanvas != null)
+        {
+            countdownCanvas.enabled = false;
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (countdownCanvas != null && countdownCanvas.enabled && Camera.main != null)
+        {
+            countdownCanvas.transform.LookAt(Camera.main.transform);
+            countdownCanvas.transform.Rotate(0, 180f, 0);
+        }
+
+    }
 
     public void ActiveLiquid()
     {
-        rend.enabled = true;   
+        if (!isCountdownActive)
+        {
+            StartCoroutine(CountdownAndActivate());
+        }
+    }
+
+    private IEnumerator CountdownAndActivate()
+    {
+        isCountdownActive = true;
+
+        if (countdownCanvas != null)
+        {
+            countdownCanvas.enabled = true;
+        }
+
+        float countdown = 10f;
+
+        while (countdown > 0)
+        {
+            int currentNumber = Mathf.CeilToInt(countdown);
+            if (countdownImage != null && countdownSprites != null && countdownSprites.Length >= currentNumber)
+            {
+                countdownImage.sprite = countdownSprites[10 - currentNumber];
+            }
+
+            yield return null;
+            countdown -= Time.deltaTime;
+        }
+
+        if (countdownCanvas != null)
+        {
+            countdownCanvas.enabled = false;
+        }
+
+        rend.enabled = true;
+        isCountdownActive = false;
     }
 }
